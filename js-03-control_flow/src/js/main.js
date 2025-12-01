75d839a07affbff427325b1c221562502e5a5a7e
z-erik <erikp2913@gmail.com>
2025-11-30 19:52:42 -0600

    feat: boton para cambiar la emocion del dinosaurio

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -27,65 +27,34 @@
 
 
 
-/*let num1=8;
-let num2=8;
-const calculate= (a,b,c)=>{
 
-    return c(a,b);
-} 
+//asignamos el valor del id a una variable const
+const dino=document.getElementById("dinosaurio");
+//valor boleano para determinar la felicidad
+let felicidad=true;
 
-const Suma =(x,y)=> x+y;
-const Resta=(a,b)=>a-b;
+//construccion del boton
+const boton= document.getElementById("boton");
 
-console.log(calculate(num1,num2,Suma));*/
+boton.onclick= function(){
+   if (felicidad) {
+    felicidad=false;
+    
+    dino.src="./public/images/dino-emo.png"
+    
+   } else {
+    felicidad=true;
+     dino.src="./public/images/dino.jpg"
+   }
 
-//console.log(false && "Hola"); // false
-
-
-/*Operador || (OR)
-- Evalúa de izquierda a derecha.
-- Si el primer valor es truthy, se detiene ahí y devuelve ese valor.
-- Si el primer valor es falsy, evalúa el segundo y devuelve ese.*/
-
-
-
-
-
-
-
-console.log(true || "Hola");     // "true"   → porque null es falsy
-console.log(null || 0);          // 0        → devuelve el primer valor truthy
-console.log(null || false);      // false    → ambos falsy, devuelve el último
-console.log(null || null);       // null     → ambos falsy
-
-/*Operador && (AND)
-- Evalúa de izquierda a derecha.
-- Si el primer valor es falsy, se detiene ahí y devuelve ese valor.
-- Si el primer valor es truthy, evalúa el segundo y devuelve ese.*/
-
-const Suma =(x,y)=> x+y;
-
-console.log(null && "Hola"); //null
-console.log(Suma(8,9) && 0); //0
-console.log(true && "Hello"); //Hello
-console.log(0 && null); //0
-console.log("Texto" && null); //null
-console.log(undefined && "Mundo"); //undefined
-console.log(Suma() && "Adiós"); //Nan
+    
+};
+    
 
 
-/*
-console.log(null && "Hola"); //Null
-console.log("" && 0);         // 0 y false o ""
-console.log(true && "Hello");       //Hello
-console.log(0 && null);       //0 
-console.log(undefined && "Hola"); //undefined
-console.log("true" && 0); //*/
-/*console.log(true && "Hello");
-console.log(0 && null);
-console.log("Texto" && null);
-console.log(undefined && "Mundo");
-console.log(false && "Adiós");
-console.log("Hola" && "Mundo");
-console.log(1 && "Activo");
-console.log("Activo" && 123);*/
+
+
+
+
+
+
